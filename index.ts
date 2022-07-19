c978ba76307b7c60cb9e748a4bfd6862257a791b
Marcus <marcus@lastcraft.com>
2022-07-19 20:38:09 +0100

    Testing mongoose defaults in constructors

--- index.ts
+++ index.ts
@@ -1,13 +1,19 @@
 import mongoose from 'mongoose';
 
 const personSchema = new mongoose.Schema({
-  name: String,
+  name: {type: String, required: true},
+  nickname: {type: String, default: ''},
+  friends: {type: [String]},
 });
 
 const Person = mongoose.model('Person', personSchema);
 
+const clearMe  = async () => {
+  await Person.deleteMany({});
+};
+
 const createMe = async () => {
-  await new Person({name: 'Marcus'}).save()
+  await new Person({name: 'Marcus', friends: ['Aviva']}).save()
 };
 
 const findMe = async () => {
@@ -17,6 +23,7 @@ const findMe = async () => {
 const main = async () => {
   await mongoose.connect('mongodb://localhost:27017/playpen');
   console.log('Connected');
+  await clearMe();
   await createMe();
   const person = await findMe();
   await mongoose.disconnect();
